da62076175873ef8cee981fac2fd7b3c0238bb8f
unique211 <vishal.rkcet@gmail.com>
2020-01-08 19:12:01 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(config('app.debug')!=true) {
-    \URL::forceScheme('https');
+             \URL::forceScheme('https');
+        }
     }
 }
